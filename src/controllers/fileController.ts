6673488000daacd16ae4b2b3e74ea2a7da3aa4dc
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2025-01-15 20:02:15 -0300

    feat: search steam db requests

--- src/controllers/fileController.ts
+++ src/controllers/fileController.ts
@@ -1,94 +1,99 @@
 import { Response } from "express";
 import { MulterRequest } from "../interfaces/MulterRequest";
+import fs from 'fs';
+import searchSteamDb from "../functions/searchSteamDb";
 
 export const uploadFile = async (req: MulterRequest, res: Response) => {
     if (!req.file) {
         return res.status(400).send('Nenhum arquivo enviado.');
     }
     
-    return res.status(400).json(req.file);
-    // return res.status(400).json(JSON.stringify(req.file));
+    // return res.status(400).json(req.file);
+    
+    const horaAtual = new Date();
+    const options = { timeZone: 'America/Sao_Paulo', hour12: false };
+    const hora1 = horaAtual.toLocaleTimeString('pt-BR', options);
+    
+    // const hora1 = new Date().toLocaleTimeString();
+    
+    let gamesToSearch = [], responseFile = '', priceGamivo, priceG2A, priceKinguin;
+    const filePath = req.file.path;
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    
+    const lines = fileContent.split('\n');
+    
+    const minPopularity = Number(lines[0]); // Primeira posição será a popularidade mínima
+    lines.shift(); // Iremos retirar a popularidade do array
+    
+    // Iterar sobre as linhas e armazenar o conteúdo no array gamesToSearch
+    for (const line of lines) {
+        // Remover espaços em branco no início e no final de cada linha
+        const trimmedLine: string = line.trim();
+        
+        // Verificar se a linha não está vazia
+        if (trimmedLine !== '') {
+            //@ts-ignore
+            gamesToSearch.push(trimmedLine);
+        }
+    }
+    
+    // O for vai passar em todos os gamesToSearch
+    for (let game of gamesToSearch) {
+        let search = true, fullLine, popularity;
+        console.log("Game: " + game);
+        popularity = await searchSteamDb(game);
+        return res.status(200).json(popularity);
+        // return res.status(200).json(popularity);
 
-    // const horaAtual = new Date();
-    // const options = { timeZone: 'America/Sao_Paulo', hour12: false };
-    // const hora1 = horaAtual.toLocaleTimeString('pt-BR', options);
+        minPopularity !== 0 ? popularity = await searchSteamDb(game) : popularity = 999;
+        // let popularity = 2442; // Debug
 
-    // // const hora1 = new Date().toLocaleTimeString();
+        if (popularity == 'F') continue;
 
-    // let gamesToSearch = [], responseFile = '', priceGamivo, priceG2A, priceKinguin;
-    // const filePath = req.file.path;
-    // const fileContent = fs.readFileSync(filePath, 'utf8');
+        if (popularity < minPopularity) search = false;
 
-    // const lines = fileContent.split('\n');
+        //Converte para o formato brasileiro com .
+        // CHECAR SE É Number
+        if (!isNumber(popularity)) {
+            if (popularity.includes(',')) { popularity = popularity.replace(',', '.'); }
+            let popularityNumber = parseFloat(popularity);
+            let decimalPlaces = (popularity.split('.')[1] || '').length;
+            popularity = popularityNumber.toFixed(decimalPlaces);
+        }
 
-    // const minPopularity = Number(lines[0]); // Primeira posição será a popularidade mínima
-    // lines.shift(); // Iremos retirar a popularidade do array
+        if (popularity <= 0 || !search) continue;
 
-    // // Iterar sobre as linhas e armazenar o conteúdo no array gamesToSearch
-    // for (const line of lines) {
-    //     // Remover espaços em branco no início e no final de cada linha
-    //     const trimmedLine = line.trim();
+        priceGamivo = await searchGamivo(game, minPopularity, popularity);
+        if (priceGamivo !== 'F' && priceGamivo !== 'N') {
+            fullLine = `G2A\t${priceGamivo}\tKinguin\t\t\t\t${popularity}\t${game}\n`;
+            responseFile += fullLine;
+        }
 
-    //     // Verificar se a linha não está vazia
-    //     if (trimmedLine !== '') {
-    //         gamesToSearch.push(trimmedLine);
-    //     }
-    // }
+        console.log(fullLine);
+    }
 
-    // console.log(gamesToSearch);
-    // // O for vai começar aqui passando em todos os gamesToSearch
-    // for (let game of gamesToSearch) {
-    //     let search = true, fullLine, popularity;
-    //     console.log("Game: " + game);
-    //     minPopularity !== 0 ? popularity = await searchSteamDb(game) : popularity = 999;
-    //     // let popularity = 2442; // Debug
+    console.log("responseFile:\n" + responseFile);
 
-    //     if (popularity == 'F') continue;
-
-    //     if (popularity < minPopularity) search = false;
-
-    //     //Converte para o formato brasileiro com .
-    //     // CHECAR SE É Number
-    //     if (!isNumber(popularity)) {
-    //         if (popularity.includes(',')) { popularity = popularity.replace(',', '.'); }
-    //         let popularityNumber = parseFloat(popularity);
-    //         let decimalPlaces = (popularity.split('.')[1] || '').length;
-    //         popularity = popularityNumber.toFixed(decimalPlaces);
-    //     }
-
-    //     if (popularity <= 0 || !search) continue;
-
-    //     priceGamivo = await searchGamivo(game, minPopularity, popularity);
-    //     if (priceGamivo !== 'F' && priceGamivo !== 'N') {
-    //         fullLine = `G2A\t${priceGamivo}\tKinguin\t\t\t\t${popularity}\t${game}\n`;
-    //         responseFile += fullLine;
-    //     }
-
-    //     console.log(fullLine);
-    // }
-
-    // console.log("responseFile:\n" + responseFile);
-
-    // // res.json('A'); // DEBUG
-    // // return;
+    // res.json('A'); // DEBUG
+    // return;
 
 
-    // fs.writeFileSync(filePath, responseFile);
+    fs.writeFileSync(filePath, responseFile);
 
-    // res.download(filePath, 'resultado-price-researcher.txt', (err) => {
-    //     // Verifica se houve algum erro durante o download
+    res.download(filePath, 'resultado-price-researcher.txt', (err) => {
+        // Verifica se houve algum erro durante o download
 
-    //     const newHoraAtual = new Date();
-    //     const options = { timeZone: 'America/Sao_Paulo', hour12: false };
-    //     const hora2 = newHoraAtual.toLocaleTimeString('pt-BR', options);
+        const newHoraAtual = new Date();
+        const options = { timeZone: 'America/Sao_Paulo', hour12: false };
+        const hora2 = newHoraAtual.toLocaleTimeString('pt-BR', options);
 
-    //     console.log(`Horário de início: ${hora1}, horário de término: ${hora2}`);
-    //     if (err) {
-    //         console.error('Erro ao fazer o download do arquivo:', err);
-    //         fs.unlinkSync(filePath);
-    //     } else {
-    //         // Se o download for bem-sucedido, exclui o arquivo
-    //         fs.unlinkSync(filePath);
-    //     }
-    // });
+        console.log(`Horário de início: ${hora1}, horário de término: ${hora2}`);
+        if (err) {
+            console.error('Erro ao fazer o download do arquivo:', err);
+            fs.unlinkSync(filePath);
+        } else {
+            // Se o download for bem-sucedido, exclui o arquivo
+            fs.unlinkSync(filePath);
+        }
+    });
 }
---
+++ src/functions/searchSteamDb.ts
@@ -0,0 +1,188 @@
+import dotenv from 'dotenv'; // Usar require para dotenv
+dotenv.config(); // Carregar variáveis de ambiente
+
+// Importações locais usando import
+import clearString from './helpers/clearString.js'; // Assumindo que estes são módulos JS no mesmo diretório
+import clearRomamNumber from './helpers/clearRomamNumber.js';
+import clearDLC from './helpers/clearDLC.js';
+import clearEdition from './helpers/clearEdition.js';
+import axios, { AxiosResponse } from 'axios';
+import * as cheerio from 'cheerio';
+
+const searchSteamDb = async (gameString): Promise<number> => {
+    let gameStringClean: string = clearRomamNumber(gameString);
+    gameStringClean = clearDLC(gameStringClean);
+    gameStringClean = clearEdition(gameStringClean);
+
+    let response: AxiosResponse | undefined;
+
+    try {
+        response = await axios.get('https://steamcharts.com/search/', {
+            params: { q: gameString }
+        });
+
+    } catch (error) {
+        // console.error('Erro ao buscar no SteamDb:', error);
+        return 0;
+    }
+
+    if (!response || !response.data) return 0;
+
+    gameString = clearDLC(gameString);
+    gameString = clearEdition(gameString);
+    gameString = clearString(gameString);
+    gameString = gameString.toLowerCase();
+    gameString = gameString.trim();
+
+    const $search = cheerio.load(response.data);
+    const links: { href: string; text: string }[] = [];
+
+    $search('a').each((_, element) => {
+        const href = $search(element).attr('href'); // Obtém o atributo href de cada <a>
+        const text = $search(element).text().trim(); // Obtém o texto dentro da tag <a> e remove espaços em branco
+        if (href && text) {
+            links.push({ href, text });
+        }
+    });
+
+    let id: string = '';
+
+    for (const link of links) {
+        let gameName = clearString(link.text);
+        gameName = clearDLC(gameName);
+        gameName = clearEdition(gameName).trim();
+
+        if (gameName === gameString) {
+            id = link.href;
+            break; // Finaliza o loop pois encontrou o elemento
+        }
+    }
+
+    if (id == '') return 0;
+
+
+    try {
+        response = await axios.get(`https://steamcharts.com${id}`);
+    } catch (error) {
+        // console.error('Erro ao buscar no SteamDb (segunda requisição):', error);
+        return 0;
+    }
+
+    if (!response || !response.data) return 0;
+
+
+    const $details = cheerio.load(response.data);
+    const spans: string[] = [];
+
+    $details('span.num').each((_, element) => {
+        const numText = $details(element).text().trim(); // Pega o texto do span e remove espaços extras
+        if (numText) {
+            spans.push(numText); // Adiciona o texto ao array
+        }
+    });
+
+    if (spans.length < 1) return 0;
+
+    return Number.parseInt(spans[1]); // Retorna o pico de jogadores nas últimas 24h
+
+
+
+
+
+
+    // let browser;
+    // try {
+    //     browser = await puppeteer.launch({
+    //         headless: true,
+    //         args: ['--no-sandbox', '--disable-setuid-sandbox']
+    //     });
+
+    //     const page = await browser.newPage();
+
+    //     // const context = await browser.createBrowserContext();
+
+    //     // const page = await context.newPage();
+
+    //     await page.setViewport({
+    //         width: 426,
+    //         height: 240
+    //     });
+
+    //     await page.goto(`https://steamcharts.com/`);
+
+    //     await page.waitForSelector('input[placeholder="search games"]');
+
+    //     // Seletor do elemento de entrada
+    //     const inputElement = await page.waitForSelector('input[placeholder="search games"]', { visible: true });
+
+
+    //     let gameStringClean = clearRomamNumber(gameString);
+    //     gameStringClean = clearDLC(gameStringClean);
+    //     gameStringClean = clearEdition(gameStringClean);
+    //     // gameStringClean = gameStringClean.trim();
+
+    //     // console.log("gameStringClean: " + gameStringClean);
+
+
+    //     // Digita o nome do jogo no elemento de entrada
+    //     await inputElement.type(gameStringClean);
+    //     await inputElement.press('Enter');
+
+    //     await page.waitForNavigation();
+
+    //     const links = await page.$$('a');
+
+    //     gameString = clearDLC(gameString);
+    //     gameString = clearEdition(gameString);
+    //     gameString = clearString(gameString);
+    //     gameString = gameString.toLowerCase();
+    //     gameString = gameString.trim();
+
+    //     // console.log("gameString: " + gameString)
+
+    // for (const link of links) {
+    //     let gameName = await page.evaluate(el => el.textContent, link);
+
+    //     gameName = clearString(gameName);
+    //     gameName = clearDLC(gameName);
+    //     gameName = clearEdition(gameName);
+    //     gameName = gameName.trim();
+    //     // console.log("gameName: " + gameName);
+
+    //     // assassin's creed chronicles russia
+    //     // assassin’s creed chronicles russia
+    //     // console.log("gameName: " + gameName); // Debug
+    //     // console.log("gameString: " + gameString);
+
+    //     if (gameName == gameString) {
+    //         await link.click();
+    //         break; // Finaliza o loop pois encontrou o elemento
+    //     }
+    // }
+
+    //     await page.waitForSelector('span.num', { timeout: timeOut });
+    //     const spans = await page.$$('span.num');
+
+    //     const popularity = await page.evaluate(span => span.textContent.trim(), spans[1]);
+    //     // console.log('Pico em 24h:', popularity);
+
+    //     // await new Promise(resolve => setTimeout(resolve,  10000)); // Debug, espera 300 segundos para depois fechar o navegador
+
+    //     return popularity;
+    // } catch (error) {
+    //     console.log(error);
+    //     return "F";
+    // } finally {
+    //     const pages = await browser.pages();
+    //     if (pages) await Promise.all(pages.map((page) => page.close()));
+    //     const childProcess = browser.process()
+    //     if (childProcess) {
+    //         childProcess.kill()
+    //     }
+    //     await browser.close();
+    //     if (browser && browser.process() != null) browser.process().kill('SIGINT');
+
+    // }
+};
+
+export default searchSteamDb;
