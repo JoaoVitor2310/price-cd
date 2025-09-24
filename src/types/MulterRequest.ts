1a67a3ddfa363e558062892ddb861b5bb741d00a
Daniel Tostes <tostes.dev@gmail.com>
2025-09-24 01:29:09 -0300

    fix: MulterRequest interface

--- src/types/MulterRequest.ts
+++ src/types/MulterRequest.ts
@@ -2,5 +2,7 @@ import type { Request } from "express";
 
 export interface MulterRequest extends Request {
 	file?: Express.Multer.File; // Para upload único
-	files?: Express.Multer.File[]; // Caso suporte múltiplos arquivos
+	files?:
+		| Express.Multer.File[]
+		| { [fieldname: string]: Express.Multer.File[] }; // Caso suporte múltiplos arquivos
 }
