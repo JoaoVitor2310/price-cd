9ff109d2f9fb6b47c99401ae7c0c102bc2108f62
Daniel Tostes <tostes.dev@gmail.com>
2025-09-25 10:21:20 -0300

    refactor: clean up comments and imports

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,11 +1,8 @@
-// src/routes/index.ts
+import { uploadFile } from "@/controllers/file-controller";
 import { Router } from "express";
-import uploadFile from "./upload-file.js";
 
 const router = Router();
 
-router.use(uploadFile); // Adiciona a rota de atualização de ofertas
-// router.use(priceResearcherRoute); // Adiciona a rota de buscador de preços
-// router.use(whenToSellRoute); // Adiciona a rota de quando vender
+router.use(uploadFile);
 
 export default router;
--- src/routes/upload-file.ts
+++ src/routes/upload-file.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 import { uploadFile } from "@/controllers/file-controller";
 
-const upload = multer({ dest: "../uploads/" }); // Configura o middleware do multer
+const upload = multer({ dest: "../uploads/" });
 const router = Router();
 router.post("/upload", upload.single("fileToUpload"), uploadFile);
 
